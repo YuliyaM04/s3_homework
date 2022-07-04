48b2f59d3d426c82e84a6a6b9dcce40855665dff
Yuliya <fokju@yandex.ru>
2022-07-04 17:02:29 +0300

    newЗадача23

--- task23/Program.cs
+++ task23/Program.cs
@@ -2,12 +2,27 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+// вычисления значения c Math.Pow
 Console.Clear();
 Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
 int i = 1;
 while (i <= num)
 {
-    Console.WriteLine($"{i} | {i*i*i}");
-    i++;
+ Console.WriteLine($"{i} | {Math.Pow(i, 3)}");
+i++;
 }
+
+// Решение с i*i*i
+// Console.Clear();
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+// while (i <= num)
+// {
+//     Console.WriteLine($"{i} | {i*i*i}");   
+//     i++;
+// }
+
+
+
